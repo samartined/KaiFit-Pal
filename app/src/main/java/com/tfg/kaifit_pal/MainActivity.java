edf3bbcd0ee2b995abf21ca3b47f8eaae8b466e3
Edgar <espinosamartinedgar@gmail.com>
2024-02-28 21:11:05 +0100

    More edittext components have been created to take user measurements. It remains to enter the hip measurement and configure it so that the button is only usable if the female gender is checked. Several container layouts have been changed to facilitate the placement of the buttons.

--- app/src/main/java/com/tfg/kaifit_pal/MainActivity.java
+++ app/src/main/java/com/tfg/kaifit_pal/MainActivity.java
@@ -2,7 +2,11 @@ package com.tfg.kaifit_pal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView tituloCalculadora = findViewById(R.id.tituloCalculadora);
+        Shader textShader = new LinearGradient(0, 0, tituloCalculadora.getPaint().measureText("Calculadora KaiFit"), 0,
+                new int[]{Color.BLUE, Color.GREEN, Color.GREEN, Color.BLUE},
+                new float[]{0, 0.5f, 0.7f, 1}, Shader.TileMode.CLAMP);
+        tituloCalculadora.getPaint().setShader(textShader);
+
+
     }
 }
