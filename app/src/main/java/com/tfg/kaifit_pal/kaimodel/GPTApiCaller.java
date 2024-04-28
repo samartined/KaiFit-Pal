972af8f39cb7ad722b76ba4f3c0874774317df85
Edgar <espinosamartinedgar@gmail.com>
2024-04-28 19:40:40 +0200

    feature: filter words at new class to avoid prompts no related with the model finality

--- app/src/main/java/com/tfg/kaifit_pal/kaimodel/GPTApiCaller.java
+++ app/src/main/java/com/tfg/kaifit_pal/kaimodel/GPTApiCaller.java
@@ -26,37 +26,31 @@ import com.tfg.kaifit_pal.views.fragments.kaiq.KaiQ;
  * This class is responsible for making API calls to the GPT-3 model.
  */
 public class GPTApiCaller {
+
     private final ChatHistoryManager chatHistoryManager;
     public static final MediaType JSON = MediaType.get("application/json; charset=utf-8");
     private static final Logger LOGGER = Logger.getLogger(GPTApiCaller.class.getName());
     private final OkHttpClient client;
     private final KaiQ kaiQ;
 
-    /**
-     * Constructor for the GPTApiCaller class.
-     *
-     * @param kaiQ An instance of the KaiQ class.
-     */
     public GPTApiCaller(KaiQ kaiQ) {
         this.kaiQ = kaiQ;
         this.chatHistoryManager = new ChatHistoryManager();
         client = new OkHttpClient();
     }
 
-    /**
-     * This method makes a request to the GPT-3 API with the user query.
-     *
-     * @param query The user query.
-     */
     public void gptApiRequest(String query) {
+        if (!isNutritionRelated(query)) {
+            kaiQ.addResponseToChat("Lo siento, " +
+                    "solo puedo responder preguntas relacionadas con la nutrición deportiva.");
+            return;
+        }
+
         JSONObject jsonObject = new JSONObject();
         try {
             jsonObject.put("model", "gpt-3.5-turbo");
             if (chatHistoryManager.getChatHistory().isEmpty()) {
-                chatHistoryManager.addAssistantMessageToHistory("Eres Kai-Q, debes actuar como un nutricionista deportivo real, " +
-                        "inteligente y conciso con sus respuestas. " +
-                        "Eres un asistente que complementa al uso de una calculadora nutricional. " +
-                        "Das la información concreta y breve que te pide el usuario, y solo amplias tu explicación si requiere un conocimiento muy técnico.");
+                chatHistoryManager.addAssistantMessageToHistory("Eres Kai-Q, debes actuar como un nutricionista deportivo real, inteligente y conciso con sus respuestas. Das la información concreta que te pide el usuario, y solo amplias tu explicación si requiere un conocimiento muy técnico.");
             }
             chatHistoryManager.addUserMessageToHistory(query);
             jsonObject.put("messages", new JSONArray(chatHistoryManager.getChatHistory()));
@@ -65,7 +59,6 @@ public class GPTApiCaller {
         } catch (JSONException e) {
             LOGGER.log(Level.SEVERE, "Error creating JSON object for GPT API request", e);
         }
-
 
         RequestBody body = RequestBody.create(jsonObject.toString(), JSON);
         Request request = new Request.Builder()
@@ -76,21 +69,11 @@ public class GPTApiCaller {
                 .build();
 
         client.newCall(request).enqueue(new Callback() {
-            /**
-             * This method is called when the request to the GPT-3 API fails.
-             * @param call The failed Call.
-             * @param e The IOException that occurred during the request.
-             */
             @Override
             public void onFailure(@NonNull Call call, @NonNull IOException e) {
                 kaiQ.addResponseToChat("Error al conectar con el servidor" + e.getMessage());
             }
 
-            /**
-             * This method is called when the request to the GPT-3 API is successful.
-             * @param call The successful Call.
-             * @param response The Response from the GPT-3 API.
-             */
             @Override
             public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
                 assert response.body() != null;
@@ -112,5 +95,17 @@ public class GPTApiCaller {
                 }
             }
         });
+    }
+
+    public boolean isNutritionRelated(@NonNull String query) {
+        String lowerCaseQuery = query.toLowerCase();
+        String[] nutritionKeywords = NutritionKeywords.getKeywords();
+
+        for (String keyword : nutritionKeywords) {
+            if (lowerCaseQuery.contains(keyword)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
---
+++ app/src/main/java/com/tfg/kaifit_pal/kaimodel/NutritionKeywords.java
@@ -0,0 +1,44 @@
+package com.tfg.kaifit_pal.kaimodel;
+
+public class NutritionKeywords {
+
+    public static String[] getKeywords() {
+        return new String[]{
+                "nutrición", "dieta", "alimentación", "calorías", "proteínas", "carbohidratos", "grasas",
+                "vitaminas", "minerales", "entrenamiento", "ejercicio", "nutrientes", "suplementos",
+                "hidratación", "recuperación", "ciclismo", "correr", "natación", "musculación", "pesas",
+                "rendimiento deportivo", "plan de alimentación", "comida pre-entrenamiento", "comida post-entrenamiento",
+                "planificación de comidas", "snacks saludables", "perder peso", "ganar músculo", "definición muscular",
+                "aumentar energía", "mejorar resistencia", "reducir fatiga", "quemar grasa", "ganar fuerza",
+                "incrementar masa muscular", "mejorar rendimiento", "alimentación equilibrada", "dieta balanceada",
+                "nutrición adecuada", "control de peso", "planificación nutricional", "hidratos de carbono",
+                "consumo de proteínas", "consumo de líquidos", "bajar porcentaje de grasa", "aumentar masa magra",
+                "mejorar composición corporal", "regeneración muscular", "control de calorías", "definir músculos",
+                "entrenamiento de resistencia", "entrenamiento de fuerza", "entrenamiento funcional", "entrenamiento cardiovascular",
+                "ejercicios de flexibilidad", "suplementación deportiva", "nutrición personalizada", "planificación de macros",
+                "control de macronutrientes", "planificación de micronutrientes", "control de porciones", "estrategias de nutrición",
+                "plan de alimentación para deportistas", "recomendaciones nutricionales",
+                "batidos proteicos", "barras energéticas", "recetas saludables", "dieta vegana", "dieta vegetariana",
+                "dietas especiales para deportistas", "metabolismo basal", "quemadores de grasa", "refuerzo muscular",
+                "estrategias de recuperación", "planificación de entrenamiento", "nutrición y salud", "evaluación física",
+                "suplementación natural", "alimentos integrales", "alimentos procesados", "marcas de suplementos",
+                "rutinas de ejercicios", "estiramientos adecuados", "alimentación balanceada", "consumo de fibra",
+                "control de azúcar en sangre", "comida para después del entrenamiento", "comida para antes del entrenamiento",
+                "alimentación para mejorar el rendimiento", "alimentación para el crecimiento muscular", "alimentación para la definición",
+                "dieta cetogénica", "nutrición deportiva", "planes de comida", "alimentación saludable", "cuidado nutricional",
+                "entrenador personal", "programa de entrenamiento", "rendimiento físico", "hidratación adecuada", "dieta equilibrada",
+                "nutricionista deportivo", "dieta baja en carbohidratos", "batidos de proteínas", "comida saludable", "hidratación óptima",
+                "necesidades nutricionales", "preparación física", "comida energética", "recuperación muscular", "plan de dieta",
+                "suplementos nutricionales", "gestión del peso", "salud y nutrición", "cuidado del cuerpo", "desarrollo muscular",
+                "alimentos ricos en proteínas", "control de la dieta", "estrategias nutricionales", "rendimiento atlético", "ejercicio físico",
+                "nutrición adecuada para deportistas", "plan de comidas saludables", "batidos para después del ejercicio", "nutrición personal",
+                "consumo de agua", "cómo perder peso", "alimentación balanceada para deportistas", "entrenamiento efectivo", "plan de nutrición",
+                "alimentos saludables", "alimentación equilibrada para deportistas", "consejos de nutrición", "plan de alimentación equilibrada",
+                "necesidades calóricas", "plan de alimentación saludable", "suplementación deportiva segura", "nutrición deportiva avanzada",
+                "dieta para aumentar masa muscular", "estrategias de hidratación", "alimentación adecuada para deportistas", "dieta para deportistas",
+                "nutrición deportiva de élite", "gestión del rendimiento", "alimentación deportiva adecuada", "nutrición para corredores",
+                "nutrición para ciclistas", "plan de nutrición personalizado", "alimentación saludable para atletas", "nutrición para triatletas",
+                "alimentación para maratonistas", "estrategias de alimentación saludable", "planificación nutricional deportiva"
+        };
+    }
+}
